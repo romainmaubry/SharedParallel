cf4150cd3bf621d415b2ad972da35eb56dcc3fef
Romain Aubry <romain@luminarycloud.com>
2024-04-17 09:04:37 -0400

    Add lock free stack

--- shared_stack.h
+++ shared_stack.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stack>
 #include <mutex>
+#include <atomic>
 
 template<class T>
 class SharedStack{
@@ -33,3 +34,47 @@ template<class T> void SharedStack<T>::push(const T &value){
   std::lock_guard<std::mutex> lock(m);
   stack.emplace(value);
 }
+
+template<class T>
+class LfStack{
+  private:
+    struct node{
+      T value;
+      node* next;
+      node(const T &val):value(val){}
+
+    };
+    std::atomic<node*> head;
+
+  public:
+    T pop();
+    void push(const T& value);
+    size_t size();
+
+  private:
+
+    std::mutex m;
+    std::stack<T> stack;
+
+};
+
+template<class T> size_t LfStack<T>::size(){
+  std::lock_guard<std::mutex> lock(m);
+  return stack.size(); 
+}
+
+template<class T> T LfStack<T>::pop(){
+  node* old_head=head.load();
+  while(old_head!=nullptr && !head.compare_exchange_weak(old_head,old_head->next));
+  if(old_head==nullptr)throw;
+  T tmp=old_head->value;
+  delete old_head;
+  return std::move(tmp);
+}
+
+template<class T> void LfStack<T>::push(const T &value){
+  node* new_node=new node(value);
+  new_node->next=head.load();
+  while(!head.compare_exchange_weak(new_node->next,new_node));
+}
+
--- test.cpp
+++ test.cpp
@@ -7,21 +7,23 @@
 #include "shared_stack.h"
 
 // Modify the stack
-void ModifyStack(SharedStack<double> &stack,size_t ithread){
+template<typename T>
+void ModifyStack(T &stack,size_t ithread){
   //std::cout<<"From thread:"<<ithread<<std::endl;	
   stack.push(5.0);
   stack.pop();
 }
 
 //Test thread safe stack
+template< typename T>
 void TestStack(){
-  SharedStack<double> stack;
-  size_t nthreads=50;
+  T stack;
+  constexpr size_t nthreads=50;
   std::vector<std::thread> t(nthreads);
   
   //Create threads
   for(size_t i=0;i<nthreads;i++){
-    t[i]=std::thread(ModifyStack,std::ref(stack),i);
+    t[i]=std::thread(ModifyStack<T>,std::ref(stack),i);
   }
 
   //Join threads
@@ -31,5 +33,6 @@ void TestStack(){
 }
 
 int main(int arc,char*argv[]){
-  TestStack();
+  TestStack<SharedStack<double>>();
+  TestStack<LfStack<double>>();
 }
